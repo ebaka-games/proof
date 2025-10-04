f2226f68dd75bbf65543390d089caa1ef17ce8e1
ebaka-games <ebakadev@gmail.com>
2025-10-04 21:42:39 +0300

    gh pages build

---
+++ _next/static/3L1PWlBtR_b4BizfYSeg7/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/fadab25102f483bb.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [
+      {
+        "source": "/proof/proof/:path*"
+      }
+    ],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/7df1d13e5a63c815.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,73357,e=>{e.v("https://ebaka-games.github.io/proof/_next/static/media/logo.ceb4e008.webp")}]);
